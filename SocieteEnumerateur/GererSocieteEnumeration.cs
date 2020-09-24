401acf7539efff2ddda2921e5991017b00d95214
SoniaSon <laib.sonia0602@gmail.com>
2020-09-24 14:41:28 +0200

    Ajoutez des fichiers projet.

---
+++ SocieteEnumerateur/GererSocieteEnumeration.cs
@@ -0,0 +1,47 @@
+﻿using SocieteEnumerateur.Societe;
+using System;
+using SocieteEnumerateur.Societe;
+using SocieteEnumerateur.ListeChainee;
+
+
+namespace SocieteEnumerateur
+{
+    class GererSocieteEnumeration
+    {
+        static void Main(string[] args)
+        {
+            var employe1 = new Employe("LAIB", "LAIB", 18, 1000);
+            var employe2 = new Employe("KACED ", "Yasmine", 19, 1100);
+            var employe3 = new Employe("ATHMANI ", "Darine", 20, 1200);
+            var employe4 = new Employe("AYAD ", "Ishak", 21, 1300);
+            var employe5 = new Employe("IGHIT ", "Yanis", 22, 1400);
+
+            var chef1 = new Chef("BEAUCHAMP", "David", 48, 4000, "Informatique");
+            var chef2 = new Chef("PRECIGOUT", "Pascal", 50, 3000, "Medecine");
+
+            var directeur = new Directeur("JOLIE", "Femme", 45, 3000, "MEDECINE", "Medecin");
+
+
+            //Inserer au debut de la liste 
+            var Liste = new Liste();
+            Liste.InsererDebut(employe1);
+            Liste.InsererDebut(employe2);
+            Liste.InsererDebut(employe3);
+            Liste.InsererDebut(employe4);
+            Liste.InsererDebut(employe5);
+            Liste.InsererDebut(chef1);
+            Liste.InsererDebut(chef2);
+            Liste.InsererDebut(directeur);
+
+            var ListeEnumeration = new ListeEnumeration(Liste);
+
+            while (ListeEnumeration.MoveNext())
+            {
+                Console.WriteLine(ListeEnumeration.Current().ToString());
+                Console.WriteLine(Environment.NewLine);
+
+            }
+
+        }
+    }
+}
---
+++ SocieteEnumerateur/ListeChainee/ListeEnumeration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SocieteEnumerateur.ListeChainee
+{
+    class ListeEnumeration : IEnumerator<Liste>
+    {
+        private Liste _liste;
+        private int _idCourant = 0;
+        Liste IEnumerator<Liste>.Current => throw new NotImplementedException();
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public ListeEnumeration(Liste liste)
+        {
+            _liste = liste;
+        }
+        public object Current()
+        {
+            return _idCourant;
+        }
+        public void Dispose()
+        {
+
+            _liste = null;
+            _idCourant = 0;
+        }
+
+        public bool MoveNext()
+        {
+            _idCourant++;
+            if (_idCourant > _liste.NbElement )
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Reset()
+        {
+            _idCourant = 0;
+        }
+
+    }
+}
